3870b1387eddee46ccbae6593d511dcd17e86f91
Matei Adriel <rafaeladriel11@gmail.com>
2019-03-07 23:18:25 +0200

    removed main.js

--- src/main.js
+++
@@ -1,5 +0,0 @@
-export * from "./ts/EventEmitter.js";
-export * from "./ts/Scene.js";
-export * from "./ts/SceneManager.js";
-export * from "./ts/SyncEventEmitter.js";
-//# sourceMappingURL=main.js.map

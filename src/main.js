84e2d5ffb7b263dd1cf04d1d3abb80be1427a6a2
Matei Adriel <rafaeladriel11@gmail.com>
2019-03-07 21:57:25 +0200

    Added paths

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export * from "./ts/EventEmitter.js";
+export * from "./ts/Scene.js";
+export * from "./ts/SceneManager.js";
+export * from "./ts/SyncEventEmitter.js";
+//# sourceMappingURL=main.js.map
